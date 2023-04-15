f0c8d7a5660a7e457df15ae266a0c6fa8f4f5f7f
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-04-15 12:09:15 -0400

    implement exercise 2: update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+puts "Loading first store (id = 1) from the database..."
+@store1 = Store.find(1)
+
+puts "Loading second store from the database..."
+@store2 = Store.find_by(id: 2)
+
+puts "Updating first store name..."
+@store1.update(name: "New Store Name")
+
+puts "First store's updated name: #{@store1[:name]}"
+
