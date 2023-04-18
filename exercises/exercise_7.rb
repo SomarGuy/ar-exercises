2dab22c97ffc8d14998a1bfc9a8c038221ada552
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-04-18 14:56:47 -0400

    Exercise 7: Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,16 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name)
+
+if new_store.errors.any?
+  puts "Error(s) occurred while creating the store:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+else
+  puts "Store was created successfully."
+end
