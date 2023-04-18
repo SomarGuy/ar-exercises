afafbe95b352372d7eb2affe2f5e080e7f2c2b67
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-04-18 14:28:42 -0400

    Exercise 4: Loading a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,18 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# Create three new stores
+Store.create(name: 'Surrey', annual_revenue: 224_000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1_900_000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430_000, mens_apparel: true, womens_apparel: true)
+
+# Fetch stores that carry men's apparel
+@mens_stores = Store.where(mens_apparel: true)
+
+# Loop through each store and output their name and annual revenue
+@mens_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
+
+# Fetch stores that carry women's apparel and have an annual revenue less than $1M
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: 0...1_000_000)
